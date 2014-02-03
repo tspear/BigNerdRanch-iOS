9c829729801314c4ca4142219472f78fdddd568a
Tim Spear <tvroom@TVRoom.local>
2014-02-03 10:20:08 +0000

    tidy up itemsviewcontroller
    reorganised methods.

--- Chapter10/Homepwner/Homepwner/ItemsViewController.m
+++ Chapter10/Homepwner/Homepwner/ItemsViewController.m
@@ -2,7 +2,7 @@
 //  ItemsViewController.m
 //  Homepwner
 //
-//  Created by TV Room on 24/01/2014.
+//  Created by Tim Spear on 24/01/2014.
 //  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
 //
 
@@ -26,81 +26,8 @@
     return [self init];
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return [[[BNRItemStore sharedStore] allItems] count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    //check for a reusable cell first, use that if it exists
-    UITableViewCell *cell =
-    [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
-    
-    // if there is no reusable cell of this type create a new one.
-    if (!cell){
-        cell = [[UITableViewCell alloc]
-                                 initWithStyle:UITableViewCellStyleDefault
-                               reuseIdentifier:@"UITableViewCell"];
-    }
-    // Set the text on the cell with the description of the item
-    // that is at the nth index of itmes, where n = row this cell
-    // will appear in on the tableview
-    BNRItem *p = [[[BNRItemStore sharedStore] allItems]
-                                  objectAtIndex:[indexPath row]];
-    [[cell textLabel] setText:[p description]];
-    
-    return cell;
-}
 
 
-- (void)tableView:(UITableView *)tableView
-        commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
-        forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete)
-    {
-        BNRItemStore *ps = [BNRItemStore sharedStore];
-        NSArray *items = [ps allItems];
-        BNRItem *p = [items objectAtIndex:[indexPath row]];
-        [ps removeItem:p];
-        
-        // We also remove that row from the table view with an animation
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
-                         withRowAnimation:UITableViewRowAnimationTop];
-    }
-    
-}
-
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
-{
-    [[BNRItemStore sharedStore] moveItemAtIndex:[sourceIndexPath row]
-                                        toIndex:[destinationIndexPath row]];
-}
-
-
-- (UIView *)headerView
-{
-    // If we havent loaded the headerView yet
-    if(!headerView) {
-        // Load headerView.xib
-        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
-    }
-    
-    return headerView;
-}
-
-- (UIView *)tableView:(UITableView *)tv viewForHeaderInSection:(NSInteger)sec
-{
-    return [self headerView];
-}
-
-- (CGFloat)tableView:(UITableView *)tv heightForHeaderInSection:(NSInteger)sec
-{
-    // The height of the header view should be determined from the height of the
-    // view in the XIB file
-    return [[self headerView] bounds].size.height;
-}
 
 
 - (IBAction)toggleEditingMode:(id)sender;
@@ -119,6 +46,7 @@
     }
 }
 
+
 - (IBAction)addNewItem:(id)sender;
 {
     // Create new BNRItem and add it to the store
@@ -132,5 +60,97 @@
     // Insert this new row into the table
     [[self tableView] insertRowsAtIndexPaths:[NSArray arrayWithObject:ip] withRowAnimation:UITableViewRowAnimationTop];
 }
+
+
+
+#pragma mark - UITableViewDataSource
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //check for a reusable cell first, use that if it exists
+    UITableViewCell *cell =
+    [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
+    
+    // if there is no reusable cell of this type create a new one.
+    if (!cell){
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleDefault
+                reuseIdentifier:@"UITableViewCell"];
+    }
+    // Set the text on the cell with the description of the item
+    // that is at the nth index of itmes, where n = row this cell
+    // will appear in on the tableview
+    BNRItem *p = [[[BNRItemStore sharedStore] allItems]
+                  objectAtIndex:[indexPath row]];
+    [[cell textLabel] setText:[p description]];
+    
+    return cell;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+
+- (void)tableView:(UITableView *)tableView
+    commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+    forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        BNRItemStore *ps = [BNRItemStore sharedStore];
+        NSArray *items = [ps allItems];
+        BNRItem *p = [items objectAtIndex:[indexPath row]];
+        [ps removeItem:p];
+        
+        // We also remove that row from the table view with an animation
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                         withRowAnimation:UITableViewRowAnimationTop];
+    }
+    
+}
+
+- (void)tableView:(UITableView *)tableView
+    moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+      toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    [[BNRItemStore sharedStore] moveItemAtIndex:[sourceIndexPath row]
+                                        toIndex:[destinationIndexPath row]];
+}
+
+//
+
+#pragma mark - UITableViewDelegate
+
+- (UIView *)tableView:(UITableView *)tv viewForHeaderInSection:(NSInteger)sec
+{
+    return [self headerView];
+}
+
+
+- (UIView *)headerView
+{
+    // If we havent loaded the headerView yet
+    if(!headerView) {
+        // Load headerView.xib
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
+    }
+    
+    return headerView;
+}
+
+
+- (CGFloat)tableView:(UITableView *)tv heightForHeaderInSection:(NSInteger)sec
+{
+    // The height of the header view should be determined from the height of the
+    // view in the XIB file
+    return [[self headerView] bounds].size.height;
+}
+
+//
+
 
 @end
