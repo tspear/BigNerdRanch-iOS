073636d24f576cf17f1baf417375fcf8e6f9a4fc
Tim Spear <tim@tspear.com>
2014-03-11 21:43:12 +0000

    chap14 - nscoder implementation

--- Chapter14/Homepwner/Homepwner/BNRItem.m
+++ Chapter14/Homepwner/Homepwner/BNRItem.m
@@ -105,4 +105,29 @@
     NSLog(@"Destroyed: %@",self);
 }
 
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeObject:itemName forKey:@"itemName"];
+    [aCoder encodeObject:serialNumber forKey:@"serialNumber"];
+    [aCoder encodeObject:dateCreated forKey:@"dateCreated"];
+    [aCoder encodeObject:imageKey forKey:@"imageKey"];
+    
+    [aCoder encodeInt:valueInDollars forKey:@"valueInDollars"];
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    if (self) {
+        [self setItemName:[aDecoder decodeObjectForKey:@"itemName"]];
+        [self setSerialNumber:[aDecoder decodeObjectForKey:@"serialNumber"]];
+        [self setImageKey:[aDecoder decodeObjectForKey:@"imageKey"]];
+    
+        [self setValueInDollars:[aDecoder decodeIntForKey:@"valueInDollars"]];
+        
+        dateCreated = [aDecoder decodeObjectForKey:@"dateCreated"];
+    }
+    return self;
+}
+
 @end
