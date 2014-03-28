f5104bab71bec6e55d29f0de3cf372be6347152c
Tim Spear <tim@tspear.com>
2014-03-28 17:28:05 +0000

    Chap15 - init

---
+++ Chapter15/Homepwner/Homepwner/ItemsViewController.h
@@ -0,0 +1,16 @@
+//
+//  ItemsViewController.h
+//  Homepwner
+//
+//  Created by TV Room on 24/01/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DetailViewController.h"
+
+@interface ItemsViewController : UITableViewController
+
+- (IBAction)addNewItem:(id)sender;
+
+@end
---
+++ Chapter15/Homepwner/Homepwner/ItemsViewController.m
@@ -0,0 +1,157 @@
+//
+//  ItemsViewController.m
+//  Homepwner
+//
+//  Created by Tim Spear on 24/01/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "ItemsViewController.h"
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
+@implementation ItemsViewController
+
+- (id)init
+{
+    // Call superclass designated initialiser
+    self = [super initWithStyle:UITableViewStyleGrouped];
+    if (self) {
+        UINavigationItem *n = [self navigationItem];
+        [n setTitle:@"Homepwner"];
+        
+        // Create a new bar button item that will send
+        // addNewItem: to ItemsViewController
+        UIBarButtonItem *bbi = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                                                             target:self
+                                                                             action:@selector(addNewItem:)];
+        
+        // Set this bar button item as the right item in the navigationItem
+        [[self navigationItem] setRightBarButtonItem:bbi];
+
+        [[self navigationItem] setLeftBarButtonItem:[self editButtonItem]];
+    }
+    return self;
+}
+
+-(id)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [[self tableView] reloadData];
+}
+
+
+- (IBAction)addNewItem:(id)sender;
+{
+    // Create new BNRItem and add it to the store
+    BNRItem *newItem = [[BNRItemStore sharedStore] createItem];
+    
+    DetailViewController *detailViewController = [[DetailViewController alloc] initForNewItem:YES];
+    
+    [detailViewController setItem:newItem];
+    [detailViewController setDismissblock:^{
+        [[self tableView] reloadData];
+    }];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:detailViewController];
+    
+    [navController setModalPresentationStyle:UIModalPresentationFormSheet];
+    [navController setModalTransitionStyle:UIModalTransitionStyleFlipHorizontal];
+    
+    [self presentViewController:navController animated:YES completion:nil];
+}
+
+- (void)setItem:(BNRItem *)item{
+    [[self navigationItem] setTitle:[item itemName]];
+}
+
+
+
+#pragma mark - UITableViewDataSource
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //check for a reusable cell first, use that if it exists
+    UITableViewCell *cell =
+    [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
+    
+    // if there is no reusable cell of this type create a new one.
+    if (!cell){
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleDefault
+                reuseIdentifier:@"UITableViewCell"];
+    }
+    // Set the text on the cell with the description of the item
+    // that is at the nth index of itmes, where n = row this cell
+    // will appear in on the tableview
+    BNRItem *p = [[[BNRItemStore sharedStore] allItems]
+                  objectAtIndex:[indexPath row]];
+    [[cell textLabel] setText:[p description]];
+    
+    return cell;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+
+- (void)tableView:(UITableView *)tableView
+    commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+    forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        BNRItemStore *ps = [BNRItemStore sharedStore];
+        NSArray *items = [ps allItems];
+        BNRItem *p = [items objectAtIndex:[indexPath row]];
+        [ps removeItem:p];
+        
+        // We also remove that row from the table view with an animation
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                         withRowAnimation:UITableViewRowAnimationTop];
+    }
+    
+}
+
+- (void)tableView:(UITableView *)tableView
+    moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+      toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    [[BNRItemStore sharedStore] moveItemAtIndex:[sourceIndexPath row]
+                                        toIndex:[destinationIndexPath row]];
+}
+
+//
+
+#pragma mark - UITableViewDelegate
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Make a new detailviewcontroller
+    DetailViewController *detailViewController = [[DetailViewController alloc] initForNewItem:NO];
+    
+    // get item pointer
+    NSArray *items = [[BNRItemStore sharedStore] allItems];
+    BNRItem *selectedItem = [items objectAtIndex:[indexPath row]];
+    
+    // give detail view controller a pointer to the item object in row
+    [detailViewController setItem:selectedItem];
+    
+    // push to the top of the navcontrollers stack
+    [[self navigationController] pushViewController:detailViewController
+                                           animated:YES];
+}
+
+//
+
+
+@end
