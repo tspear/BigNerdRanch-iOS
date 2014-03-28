5c74db2a5801d6d68b13f6ab81ccd42b5bb6d65f
Tim Spear <tim@tspear.com>
2014-03-28 17:28:42 +0000

    Chap15 - added HomepwnerItemCell class and .xib

---
+++ Chapter15/Homepwner/Homepwner/HomepwnerItemCell.h
@@ -0,0 +1,13 @@
+//
+//  HomepwnerItemCell.h
+//  Homepwner
+//
+//  Created by TV Room on 28/03/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HomepwnerItemCell : UITableViewCell
+
+@end
---
+++ Chapter15/Homepwner/Homepwner/HomepwnerItemCell.m
@@ -0,0 +1,13 @@
+//
+//  HomepwnerItemCell.m
+//  Homepwner
+//
+//  Created by TV Room on 28/03/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "HomepwnerItemCell.h"
+
+@implementation HomepwnerItemCell
+
+@end
