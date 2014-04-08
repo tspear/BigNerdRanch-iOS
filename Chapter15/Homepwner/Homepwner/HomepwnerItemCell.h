8af39180a70b26e9989cbc543c31a6459742b391
Tim Spear <tim@tspear.com>
2014-04-08 23:51:09 +0100

    chap15 - extend itemcell

--- Chapter15/Homepwner/Homepwner/HomepwnerItemCell.h
+++ Chapter15/Homepwner/Homepwner/HomepwnerItemCell.h
@@ -14,5 +14,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *serialNumberLabel;
 @property (weak, nonatomic) IBOutlet UILabel *valueLabel;
+@property (weak, nonatomic) id controller;
+@property (weak, nonatomic) UITableView *tableView;
+
+- (IBAction)showImage:(id)sender;
 
 @end
